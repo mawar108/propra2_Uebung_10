1370bff8c6b8e219f1ed1f3658118edf64bd2919
Wartisch <mawar108@uni-duessldorf.de>
2021-02-09 10:53:42 +0100

    mob next [ci-skip]

--- bestellung/src/main/java/bestellung/BestellungenService.java
+++ bestellung/src/main/java/bestellung/BestellungenService.java
@@ -1,28 +1,33 @@
 package bestellung;
 
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Queue;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class BestellungenService {
 
+  Queue<Bestellung> offeneBestellungen =new LinkedList<>();
 
   @Value("${lieferung.endpoint}")
   private String endpoint;
 
+  private final RestTemplate rest;
 
   private final BestellungRepository repository;
 
   public BestellungenService(BestellungRepository repository) {
+    this.rest = new RestTemplate();
     this.repository = repository;
   }
 
@@ -32,6 +37,7 @@ public class BestellungenService {
 
   public void neueBestellungFuer(String kunde) {
     Bestellung bestellung = new Bestellung(kunde);
+    offeneBestellungen.add(bestellung);
     repository.save(bestellung);
 
     // Hier muss das andere SCS asynchron informiert werden
@@ -42,6 +48,22 @@ public class BestellungenService {
 
   }
 
+  @Scheduled(fixedDelay = 1000)
+  public void lieferSystem(){
+    try{
+      while(!offeneBestellungen.isEmpty()){
+        MultiValueMap<String,Long> map= new LinkedMultiValueMap<String, Long>();
+        map.add("id", offeneBestellungen.peek().getId());
+        HttpEntity<MultiValueMap<String,Long>> request= new HttpEntity<>(map);
+        ResponseEntity<String> responseEntity= rest.postForEntity(endpoint, request, String.class);
+        offeneBestellungen.remove();
+      }
+    }
+    catch(RestClientException e){
+      e.printStackTrace();
+      System.out.println("Post fehlgeschlagen");
+    }
+  }
 
 
 
